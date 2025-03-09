34542162295157d361ece71025e46505df1495ac
Sayzera <white.code.text@gmail.com>
2025-03-09 13:58:29 +0300

    latests

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,15 @@ app.use(cors());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/task', taskRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: `
+      Merhaba, ben Sezer. Bu uygulama MERN Stack ile geliştirilmiştir.
+      Uygulama içerisinde kullanıcı kaydı oluşturabilir, giriş yapabilir ve task ekleyebilirsiniz.
+    ` });
+});
+
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
